da1958b3d22deb80b14b36fbaf7ace35453148a8
Shivansh Srivastava <95683373+DarkSamus669@users.noreply.github.com>
2023-11-16 11:24:29 +0530

    Update make_appimage.sh

--- scripts/make_appimage.sh
+++ scripts/make_appimage.sh
@@ -17,7 +17,7 @@ export OUTPUT="SameBoy-0.15.8-x86_64.AppImage"
 export UPDATE_INFORMATION="gh-releases-zsync|DarkSamus669|sameboy-appimage|SameBoy-0.15.8-x86_64.AppImage"
 
 ./linuxdeploy-x86_64.AppImage -i AppDir/usr/share/icons/hicolor/512x512/apps/sameboy.png \
-		-d ../FreeDesktop/sameboy.desktop \
+		-d FreeDesktop/sameboy.desktop \
 		--executable AppDir/usr/bin/sameboy \
 		--appdir AppDir \
-        	--output appimage
+  		--output appimage
